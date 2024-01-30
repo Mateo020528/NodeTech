3733adddb4b442f855fc4d68054298ebba8c713b
Mateo020528 <149026537+Mateo020528@users.noreply.github.com>
2024-01-29 21:32:01 -0500

    Se añade el boton para ingresar a la cuenta desde.
    Se agrega el boton al login que nos lleva a la pagian cuenta.

--- login/app.js
+++ login/app.js
@@ -1,17 +1,27 @@
 const url = "http://localhost:3000/users" //Utilizo la variable url para guardar el link de users
 const correo = document.getElementById("correo"); // Llamamos el id de el correo
 const password = document.getElementById("password");//Llamamos el id del password
+const iniciar = document.querySelector("#inicio") //Llamamos al boton de inicio
 
-
-fetch (url).then(response =>{return response.json()}).then(data =>{
-    data.forEach(element => {
-        console.log(element);
-      
-    });
-})
+iniciar.addEventListener("click", verificar)
 
 function verificar(){
-    if(element.correo === correo.value && element.password === password.value){
-        alert("El usuario existe en la base de gatos")
-    }
+    fetch (url).then(response =>{return response.json()}).then(data =>{
+        data.forEach(element => {
+            console.log(element)
+            
+            if(correo.value != "" && password.value != "" ){
+                if(correo.value === element.email && password.value === element.password) {
+                    alert("el usuario ha ingresado el correo ")
+                    window.location.href = "../account/index.html";
+                    sessionStorage.setItem("correo",correo.value)
+                } else {
+                    alert("Revisa bien tus datos, hay algo que ingresaste mal")
+                }
+            } else {
+                alert("por favor ingrese todos los campos requeridos")
+            }
+           
+        });
+    })
 }
