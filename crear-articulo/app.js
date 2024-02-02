c0adfd11b477b4c6135578744aa459ecb5cefd9e
Maicol <92267985+kemtch19@users.noreply.github.com>
2024-02-02 17:47:10 -0500

    actualización de headers

---
+++ crear-articulo/app.js
@@ -0,0 +1,12 @@
+ClassicEditor.create(document.querySelector('#txtDescripcion')).catch(error => {
+    console.error(error);
+});
+let root = document.getElementById('root');
+let texto = document.createElement("div");
+function ver() {
+    let df = document.getElementsByClassName('ck-editor__editable_inline');
+    console.log(df[0].innerHTML)
+    texto.innerHTML = df[0].innerHTML;
+    texto.classList.add("texto");
+    root.appendChild(texto);
+}
