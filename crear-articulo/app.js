f31588738001b06a333a200f5830d319e1ee09d6
Angelica9924 <149621614+Angelica9924@users.noreply.github.com>
2024-02-05 16:34:52 -0500

    Crear Articulo: Funcionalidad Crear

--- crear-articulo/app.js
+++ crear-articulo/app.js
@@ -1,12 +1,20 @@
-ClassicEditor.create(document.querySelector('#txtDescripcion')).catch(error => {
+ClassicEditor
+.create(document.querySelector('#txtDescripcion'))
+.catch(error => {
     console.error(error);
 });
+
 let root = document.getElementById('root');
 let texto = document.createElement("div");
 function ver() {
-    let df = document.getElementsByClassName('ck-editor__editable_inline');
-    console.log(df[0].innerHTML)
-    texto.innerHTML = df[0].innerHTML;
-    texto.classList.add("texto");
-    root.appendChild(texto);
+let df = document.getElementsByClassName('ck-editor__editable_inline');
+console.log(df[0].innerHTML)
+texto.innerHTML = df[0].innerHTML;
+texto.classList.add("texto");
+root.appendChild(texto);
+localStorage.setItem("texto", texto.innerHTML)
+}
+
+function publicar() {
+    location.href = "./index2.html"
 }
---
+++ crear-articulo/nuevoArticulo/app2.js
@@ -0,0 +1,5 @@
+const texto = localStorage.getItem("texto");
+
+
+let contenido2 = document.querySelector(".contenido2")
+contenido2.innerHTML = texto;
