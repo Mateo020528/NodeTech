35f971026f3d54645d5479c99ab50985c0f7d372
Maicol <92267985+kemtch19@users.noreply.github.com>
2024-02-05 15:45:21 -0500

    mejora del front

--- assets/traduccion.js
+++ assets/traduccion.js
@@ -4,7 +4,7 @@ const body = document.body;
 /* Traducción Español */
 const col = document.querySelector('#colombia');
 
-function traducirEs() {
+/* function traducirEs() {
     const optionsCol = {
         method: 'POST',
         headers: {
@@ -18,39 +18,16 @@ function traducirEs() {
             html: body.innerHTML
         })
     };
-
     fetch(url ,optionsCol).then(r=>r.json()).then(d=>{
         body.innerHTML=d.trans
         console.log("Traducido a Español!")
     }).catch(err => console.error("ERROR: ", err.message));
-}
-
-// col.addEventListener('click', ()=>{
-//     const optionsCol = {
-//         method: 'POST',
-//         headers: {
-//             'content-type': 'application/x-www-form-urlencoded',
-//             'X-RapidAPI-Key': 'f0352ad7cfmsh6d616f46931fdedp1d062ejsn8fe7c2d34671',
-//             'X-RapidAPI-Host': 'google-translate113.p.rapidapi.com'
-//         },
-//         body: new URLSearchParams({
-//             from: 'en',
-//             to: 'es',
-//             html: body.innerHTML
-//         })
-//     };
-
-//     fetch(url ,optionsCol).then(r=>r.json()).then(d=>{
-//         body.innerHTML=d.trans
-//         console.log("Traducido a Español!")
-//     }).catch(err => console.error("ERROR: ", err.message));
-// })
+} */
 
 /* Traducción Estados Unidos */
 const us = document.querySelector('#estados-unidos');
 
-
-function traducirEn() {
+/* function traducirEn() {
     const optionsUs = {
         method: 'POST',
         headers: {
@@ -64,30 +41,36 @@ function traducirEn() {
             html: body.innerHTML
         })
     };
-
-    fetch(url ,optionsUs).then(r=>r.json()).then(d=>{
+    fetch(url ,optionsUs).then(r=>r.json()).then(d=>{        
         body.innerHTML = d.trans
         console.log("Traducido a Inglés!")
     }).catch(err => console.error("ERROR: ", err.message));
+} */
+
+const optionsUs = {
+    method: 'POST',
+    headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'X-RapidAPI-Key': 'f0352ad7cfmsh6d616f46931fdedp1d062ejsn8fe7c2d34671',
+        'X-RapidAPI-Host': 'google-translate113.p.rapidapi.com'
+    },
+    body: new URLSearchParams({
+        from: 'es',
+        to: 'en',
+        html: body.innerHTML
+    })
+};
+fetch(url ,optionsUs).then(r=>r.json()).then(d=>{        
+    // body.innerHTML = d.trans
+    localStorage.setItem("bodyEs", body.innerHTML)
+    localStorage.setItem("bodyEn", d.trans)
+    console.log("Traducido a Inglés!")
+}).catch(err => console.error("ERROR: ", err.message));
+
+function traducirEn() {
+    body.innerHTML = localStorage.getItem("bodyEn");
 }
-// us.addEventListener('click', ()=>{
-//     const optionsUs = {
-//         method: 'POST',
-//         headers: {
-//             'content-type': 'application/x-www-form-urlencoded',
-//             'X-RapidAPI-Key': 'f0352ad7cfmsh6d616f46931fdedp1d062ejsn8fe7c2d34671',
-//             'X-RapidAPI-Host': 'google-translate113.p.rapidapi.com'
-//         },
-//         body: new URLSearchParams({
-//             from: 'es',
-//             to: 'en',
-//             html: body.innerHTML
-//         })
-//     };
 
-//     fetch(url ,optionsUs).then(r=>r.json()).then(d=>{
-//         body.innerHTML = d.trans
-//         console.log("Traducido a Inglés!")
-//     }).catch(err => console.error("ERROR: ", err.message));
-// })
-
+function traducirEs() {
+    body.innerHTML = localStorage.getItem("bodyEs");
+}
